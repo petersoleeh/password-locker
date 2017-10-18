0586047570f690902bcebe01a6b5be1421781b79
peter soleeh <petersoleeh@gmail.com>
2017-10-19 00:31:02 +0300

    test behaviours in password.py

--- password_test.py
+++ password_test.py
@@ -1,5 +1,5 @@
 import unittest
-from password import User
+from password import User, Credentials
 
 
 class TestUser(unittest.TestCase):
@@ -37,17 +37,6 @@ class TestUser(unittest.TestCase):
 
         self.assertEqual(len(User.user_list), 2)
 
-    def test_login(self):
-        '''
-        test login by inputting user name and password
-        '''
-        self.new_user.save_user()
-
-        test_user = User("Josie", "123456")
-        test_user.save_user()
-
-        user_exists = User.user_exist("Josie")
-        self.assertTrue(user_exists)
 
     def test_find_user(self):
         '''
@@ -60,6 +49,51 @@ class TestUser(unittest.TestCase):
 
         user_found = User.find_by_name("Josie")
         self.assertEqual(user_found.password, test_user.password)
+
+class TestCredentials(unittest.TestCase):
+    '''
+    test for Credentials
+    '''
+
+    def setUp(self):
+        '''
+        test setup method
+        '''
+
+        self.new_credential = Credentials("twitter","password123")
+
+    def test_init(self):
+        '''
+        test initilalization
+        '''
+        self.assertEqual(self.new_credential.account_name,"twitter")
+        self.assertEqual(self.new_credential.account_password,"password123")
+
+
+    def test_save_account(self):
+        '''
+        test to see if new credentials are saved in our credential_list
+        '''
+        self.new_credential.save_account()
+        self.assertEqual(len(Credentials.credential_list),1)
+
+    def tearDown(self):
+        '''
+        clear our credential_list
+        '''
+        Credentials.credential_list = []
+
+
+    def test_save_multiple_account(self):
+        '''
+        test ability to save more than one credentials
+        '''
+        self.new_credential.save_account()
+        test_account = Credentials("whatsapp","password456")
+        test_account.save_account()
+
+        self.assertEqual(len(Credentials.credential_list),2)
+
 
 
 if __name__ == '__main__':
