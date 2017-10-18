ff5c909cd115426d996e81f5eb6edc91b2241c82
peter soleeh <petersoleeh@gmail.com>
2017-10-19 00:29:53 +0300

    add credentials class

--- password.py
+++ password.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3.6
+import string
+import random
+
+global user_list
 class User:
     """docstring for User."""
 
@@ -24,12 +28,44 @@ class User:
             if user.user_name == name:
                 return user
 
+
+class Credentials:
+
+    '''
+    instanciate credential
+    '''
+
+    global user_list
+    credential_list =[]
+
+    def __init__(self, account_name,account_password):
+        self.account_name = account_name
+        self.account_password = account_password
+
+    def save_account(self):
+
+        """
+        save to credential_list
+        """
+        Credentials.credential_list.append(self)
+
+
+
     @classmethod
-    def user_exist(cls, name):
+    def user_exist(cls, user_name,password):
         '''
         check whether current user exist and login
         '''
         for user in cls.user_list:
-            if user.user_name == name and user.password == password:
-                return user
+            if user.user_name == user_name and user.password == password:
+                return True
         return False
+
+
+    @classmethod
+    def display_accounts(cls):
+
+        """
+        display items in the credential list
+        """
+        return cls.credential_list
